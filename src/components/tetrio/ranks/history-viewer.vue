e44359ae1a730c924d6575112acdfbf3880d6497
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-02-27 09:28:58 +0800

    feat(history): move detailed data to card

--- src/components/tetrio/ranks/history-viewer.vue
+++ src/components/tetrio/ranks/history-viewer.vue
@@ -1,84 +1,22 @@
 <script lang="ts" setup>
-import { breakpointsTailwind, createReusableTemplate, useBreakpoints } from '@vueuse/core'
-import { groupBy, last, pipe } from 'remeda'
+import { groupBy, last } from 'remeda'
 import type TetrioRank from '~/models/TetrioRank'
 
-const props = defineProps<{
+defineProps<{
 	readonly records: TetrioRank[]
 }>()
-
-const group = computed(() => {
-	return pipe(
-		props.records,
-		groupBy(record => {
-			return record.record_at.toLocaleDateString()
-		})
-	)
-})
-
-const latest = computed(() => {
-	return last(props.records)
-})
-
-const [LimitDateViewerDefine, LimitDateViewer] = createReusableTemplate<{
-	readonly type: 'minimum' | 'maximum'
-}>()
-
-const breakpoints = useBreakpoints(breakpointsTailwind)
-
-const isMobile = computed(() => {
-	return breakpoints.isSmallerOrEqual('sm')
-})
-
-const placement = computed(() => {
-	if (isMobile.value) {
-		return 'top'
-	}
-
-	return 'left'
-})
 </script>
 
 <template>
-	<n-tabs :default-value="latest.record_at.toLocaleDateString()" :placement="placement" class="max-h-100"
-			justify-content="center" type="line">
-		<template v-for="(records, date) in group">
+	<n-tabs :default-value="last(records).record_at.toLocaleDateString()" justify-content="center" type="line">
+		<template v-for="(records, date) in groupBy(records, record => record.record_at.toLocaleDateString())">
 			<n-tab-pane :name="date.toString()" :tab="date">
-				<n-tabs :default-value="latest.record_at.toString()" :placement="placement" class="h-full"
-						justify-content="center" type="line">
+				<n-tabs :default-value="last(records).record_at.toString()" class="h-full" justify-content="center"
+						type="line">
 					<template v-for="record in records">
 						<n-tab-pane :name="record.record_at.toString()" :tab="record.record_at.toLocaleTimeString()">
-							<n-flex :vertical="!isMobile" align="center" justify="center">
-								<div class="fw-bold leading-tight">
-									<n-text>{{ record.require_tr.toFixed(2) }} TR</n-text>
-									<br>
-									<n-text>{{ record.player_count }} 玩家</n-text>
-								</div>
-
-								<LimitDateViewerDefine v-slot="{ type }">
-									<n-flex :size="0" vertical>
-										<div v-for="_ in ['apm', 'pps', 'vs']" class="inline-block">
-											<n-text>最小 {{ _.toUpperCase() }}: {{ record[`${type}_${_}`] }}</n-text>
-											<n-text type="info">&nbsp;By&nbsp;</n-text>
-											<n-button
-												:href="(`https://ch.tetr.io/u/${record[`${type}_${_}_player_id`]}`)"
-												tag="a" text type="primary">
-												{{ record[`${type}_${_}_player_name`] }}
-											</n-button>
-										</div>
-									</n-flex>
-								</LimitDateViewerDefine>
-
-								<n-flex :size="0" vertical>
-									<n-text v-for="_ in ['apm', 'pps', 'vs']">
-										平均 {{ _.toUpperCase() }}: {{ record[`average_${_}`].toFixed(2) }}
-									</n-text>
-								</n-flex>
-
-								<LimitDateViewer type="minimum"/>
-								<LimitDateViewer type="maximum"/>
-
-								<n-text :depth='3' class='text-lg'>{{ record.record_at.toLocaleString() }}</n-text>
+							<n-flex justify="center">
+								<tetrio-ranks-card :record="record" class="text-center" detailed/>
 							</n-flex>
 						</n-tab-pane>
 					</template>
