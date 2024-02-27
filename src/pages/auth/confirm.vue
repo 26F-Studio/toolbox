69c7f9335cd1271dbe2a00b7a25468aeb632897c
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-02-27 09:28:42 +0800

    feat(login-confirm): add text and pulse animate

--- src/pages/auth/confirm.vue
+++ src/pages/auth/confirm.vue
@@ -13,5 +13,13 @@ watch(user, () => {
 </script>
 
 <template>
-	<n-result class="animate-spin" size="huge" status="404"/>
+	<n-flex vertical>
+		<div class="overflow-hidden">
+			<n-result class="animate-spin" size="huge" status="404"/>
+		</div>
+
+		<div class="text-center">
+			<n-text class="text-4xl fw-bold animate-(pulse duration-800)">登录中...</n-text>
+		</div>
+	</n-flex>
 </template>
