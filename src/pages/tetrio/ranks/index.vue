5f921be70483ae489fe4130487170f8075fa744b
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-02-27 09:29:24 +0800

    feat(ranks): unwrap ref, direct get data

--- src/pages/tetrio/ranks/index.vue
+++ src/pages/tetrio/ranks/index.vue
@@ -1,28 +1,24 @@
 <script lang="ts" setup>
-import { tryOnBeforeMount } from '@vueuse/core'
 import { groupBy, isDefined, last, mapValues, pipe, prop, sortBy } from 'remeda'
 import TetrioRank from '~/models/TetrioRank'
 import type { Database } from '~/types/supabase'
 
-const supabase = useSupabaseClient<Database>()
-const ranks = ref<TetrioRank[]>()
-
-tryOnBeforeMount(async () => {
-	ranks.value = await supabase.from('tetrio_ranks').select()
-		.then(result => {
-			return result.data?.map(record => {
-				return new TetrioRank(record)
-			})
+const ranks = await useSupabaseClient<Database>()
+	.from('tetrio_ranks')
+	.select()
+	.then(result => {
+		return result.data?.map(record => {
+			return new TetrioRank(record)
 		})
-})
+	})
 
 const latestRankRecord = computed(() => {
-	if (!isDefined(ranks.value)) {
+	if (!isDefined(ranks)) {
 		return
 	}
 
 	return pipe(
-		ranks.value,
+		ranks,
 		groupBy(prop('name')),
 		mapValues(sortBy(prop('record_at'))),
 		mapValues(last())
@@ -33,9 +29,7 @@ const latestRankRecord = computed(() => {
 <template>
 	<n-flex vertical>
 		<n-flex justify='center'>
-			<template v-for="record in latestRankRecord">
-				<tetrio-ranks-card :record="record"/>
-			</template>
+			<tetrio-ranks-card v-for="record in latestRankRecord" :record="record"/>
 		</n-flex>
 
 		<tetrio-ranks-chart :records="ranks" title="TR"/>
