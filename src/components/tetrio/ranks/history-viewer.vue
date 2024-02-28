0a7b426506ce7e3a89dcf80e6b1decd9efd10375
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-02-28 15:31:40 +0800

    feat(history-viewer): inline `v-for`

--- src/components/tetrio/ranks/history-viewer.vue
+++ src/components/tetrio/ranks/history-viewer.vue
@@ -9,19 +9,18 @@ defineProps<{
 
 <template>
 	<n-tabs :default-value="last(records).record_at.toLocaleDateString()" justify-content="center" type="line">
-		<template v-for="(records, date) in groupBy(records, record => record.record_at.toLocaleDateString())">
-			<n-tab-pane :name="date.toString()" :tab="date">
-				<n-tabs :default-value="last(records).record_at.toString()" class="h-full" justify-content="center"
-						type="line">
-					<template v-for="record in records">
-						<n-tab-pane :name="record.record_at.toString()" :tab="record.record_at.toLocaleTimeString()">
-							<n-flex justify="center">
-								<tetrio-ranks-card :record="record" class="text-center" detailed/>
-							</n-flex>
-						</n-tab-pane>
-					</template>
-				</n-tabs>
-			</n-tab-pane>
-		</template>
+		<n-tab-pane v-for="(records, date) in groupBy(records, record => record.record_at.toLocaleDateString())"
+					:name="date.toString()"
+					:tab="date">
+			<n-tabs :default-value="last(records).record_at.toString()" class="h-full" justify-content="center"
+					type="line">
+				<n-tab-pane v-for="record in records" :name="record.record_at.toString()"
+							:tab="record.record_at.toLocaleTimeString()">
+					<n-flex justify="center">
+						<tetrio-ranks-card :record="record" class="text-center" detailed/>
+					</n-flex>
+				</n-tab-pane>
+			</n-tabs>
+		</n-tab-pane>
 	</n-tabs>
 </template>
