f6c60c842b8529f94944f3647a369d695f46cca2
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-02-28 15:32:52 +0800

    feat(composable): add menu utils

---
+++ src/composables/menu.ts
@@ -0,0 +1,56 @@
+import { NuxtLink } from '#components'
+import { type MenuOption, NIcon, NImage } from 'naive-ui'
+import { merge, pick } from 'remeda'
+import type { Component } from 'vue'
+
+export const createMenu = (name: string, icon: Component): MenuOption => {
+	return {
+		label: name,
+		key: name,
+		icon: () => h(NIcon, {
+			component: icon
+		})
+	}
+}
+
+export const createMenuWithClassIcon = (name: string, iconClass: string): MenuOption => {
+	return merge(createMenu(name, {}), {
+		icon: () => h('div', {
+			class: `w-8 h-8 ${iconClass}`
+		})
+	})
+}
+
+export const createMenuWithImageIcon = (name: string, imageSrc: string): MenuOption => {
+	return merge(createMenu(name, {}), {
+		icon: () => h(NImage, {
+			class: 'w-full h-full',
+			src: imageSrc
+		})
+	})
+}
+
+export const createRouteMenu = (name: string, routeName: string, icon: Component): MenuOption => {
+	return merge(createMenu(name, icon), {
+		label: () => h(NuxtLink, {
+			to: {
+				name: routeName
+			}
+		}, () => name),
+		key: routeName
+	})
+}
+
+export const createRouteMenuWithClassIcon = (name: string, routeName: string, iconClass: string): MenuOption => {
+	return merge(
+		createRouteMenu(name, routeName, {}),
+		pick(createMenuWithClassIcon(name, iconClass), ['icon'])
+	)
+}
+
+export const createRouteMenuWithImageIcon = (name: string, routeName: string, imageSrc: string): MenuOption => {
+	return merge(
+		createRouteMenu(name, routeName, {}),
+		pick(createMenuWithImageIcon(name, imageSrc), ['icon'])
+	)
+}
