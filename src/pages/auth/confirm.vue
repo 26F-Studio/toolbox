9d3b9ae66b56320d6b6a0f8de69b22fc05a5eb43
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-02-28 15:30:57 +0800

    feat(auth): add error state and move states to single component

--- src/pages/auth/confirm.vue
+++ src/pages/auth/confirm.vue
@@ -10,16 +10,13 @@ watch(user, () => {
 }, {
 	immediate: true
 })
+
+const params = new URLSearchParams(location.search)
 </script>
 
 <template>
-	<n-flex vertical>
-		<div class="overflow-hidden">
-			<n-result class="animate-spin" size="huge" status="404"/>
-		</div>
-
-		<div class="text-center">
-			<n-text class="text-4xl fw-bold animate-(pulse duration-800)">登录中...</n-text>
-		</div>
-	</n-flex>
+	<transition mode="out-in" name="page">
+		<auth-confirm-waiting v-if="!params.has('error')"/>
+		<auth-confirm-error v-else :description="params.get('error_description')"/>
+	</transition>
 </template>
